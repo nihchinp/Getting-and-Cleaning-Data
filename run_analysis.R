e1a8a01126e7458c92b12ba42c88bcbe844a454b
nihchinp <48209146+nihchinp@users.noreply.github.com>
2019-04-17 14:19:14 +0800

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -35,7 +35,7 @@ str(dataSubjectTest)
 str(dataFeaturesTest)
 str(dataFeaturesTrain)
 
-###Step 3. Merges the training and the test sets to create one data set
+###Step 3. Merges the Training and the Test Sets to Create One Dataset
 
 #Concatenate the data tables by rows
 dataSubject <- rbind(dataSubjectTrain, dataSubjectTest)
@@ -52,7 +52,7 @@ names(dataFeatures)<- dataFeaturesNames$V2
 dataCombine <- cbind(dataSubject, dataActivity)
 Data <- cbind(dataFeatures, dataCombine)
 
-###Step 4. Extracts only the measurements on the mean and standard deviation for each measurement
+###Step 4. Extracts Only the Measurements on the Mean and Standard Deviation for Each Measurement
 
 #4a. Subset Name of Features by measurements on the mean and standard deviation
 subdataFeaturesNames<-dataFeaturesNames$V2[grep("mean\\(\\)|std\\(\\)", dataFeaturesNames$V2)]
@@ -64,19 +64,19 @@ Data<-subset(Data,select=selectedNames)
 #4c. Check the structures of the data frame
 str(Data)
 
-###Step 5. Uses descriptive activity names to name the activities in the data set
+###Step 5. Uses Descriptive Activity Names to Name the Activities in the Dataset
 
 #5a. Read descriptive activity names from "activity_labels.txt"
 activityLabels <- read.table(file.path(path_rf, "activity_labels.txt"),header = FALSE)
 
-#5b. Factorize Variable activity in the data frame using descriptive activity names
+#5b. Factorize variable activity in the data frame using descriptive activity names
 Data$activity<-factor(Data$activity);
 Data$activity<- factor(Data$activity,labels=as.character(activityLabels$V2))
 
 #5c. Check the outcome
 head(Data$activity,30)
 
-###Step 6. Appropriately labels the data set with descriptive variable names
+###Step 6. Labels the Dataset with Descriptive Variable Names
 
 names(Data)<-gsub("^t", "time", names(Data))
 names(Data)<-gsub("^f", "frequency", names(Data))
@@ -87,7 +87,7 @@ names(Data)<-gsub("BodyBody", "Body", names(Data))
 
 names(Data)
 
-###Step 7. Creates a second, independent tidy data set with the average of each variable for each activity and each subject
+###Step 7. Creates Additional and Independent Tidy Dataset with the Average of Each Variable for Each Activity and Each Subject
 
 library(plyr);
 Data2<-aggregate(. ~subject + activity, Data, mean)
